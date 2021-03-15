589bc67876392f826b1bf85e115ca8ae9572e73b
ahdsr <lucas.czuchraj@gmail.com>
2021-03-14 21:27:44 -0400

    Back up and running

---
+++ pages/testpage.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="mt-6 bg-gray-300">
+        <h1>Hello World!</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
