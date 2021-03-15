601c9f518bd65183eb00d75a859ce8a337864600
ahdsr <lucas.czuchraj@gmail.com>
2021-03-15 10:52:59 -0400

    Getting errors here

---
+++ components/propertyCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <div class="bg-gray-300">
+      <propertyCard :property="property" />
+    </div>
+  </div>
+</template>
+
+<script>
+import propertyCard from '~/components/propertyCard'
+
+export default {
+  name: 'app',
+  components: {
+    propertyCard,
+  },
+  data: () => ({
+    property: {
+      imageURL: '#',
+      imageAlt: 'Image Description',
+      regularPrice: '$29.99',
+      salePrice: '$19.99',
+      heading: 'Giant Bicycle',
+      reviewCount: 34,
+      raiting: '4',
+    },
+  }),
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ pages/card.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <img :src="property.imageURL" : alt="property.imageAlt" />
+    <h4>{{ property.heading }}</h4>
+    <div>{{ property.regularPrice }} / regular</div>
+    <div>{{ property.salePrice }} / sale</div>
+    <div>
+      {{ property.raiting }} / 5 starts based on {{ property.reviewCount }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['property'],
+}
+</script>
+
+<style lang="scss" scoped></style>
