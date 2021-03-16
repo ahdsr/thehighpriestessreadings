90689d3ff3ff2f2a891f884ce87cef8c97c69607
ahdsr <lucas.czuchraj@gmail.com>
2021-03-16 12:29:20 -0400

    Updates to Splash

--- pages/splash.vue
+++ pages/splash.vue
@@ -1,76 +1,128 @@
 <template>
-  <div id="gradient" class="p-16 mx-auto mb-0 bg-gray-200">
-    <div></div>
-    <div>
+  <div>
+    <div class="p-16 mx-auto mb-0 bg-gray-200">
       <img class="p-16 -mb-16 w-96" src="~/assets/images/atomic.svg" />
       <h1 class="p-16 mb-0 font-mono text-8xl">
         Lucas is a UI & UX designer located in Toronto.
       </h1>
-      <div class="p-16 text-2xl">
-        <p>
-          Lucas Czuchraj is a designer from Toronto. He has 15+ years of
-          experience in the field, graduated from the York Sheridan Bachelor of
-          Design (Honors) program. He’s interested in all aspects of design,
-          from sketching, to wireframing and final look and feel. Music and
-          photography are his other passions. Check out the resume below.
-        </p>
+      <div class="flex">
+        <div class="p-16 text-4xl leading-relaxed">
+          <p>
+            Lucas Czuchraj is a designer from Toronto. He has 15+ years of
+            experience in the field, graduated from the York Sheridan Bachelor
+            of Design (Honors) program. He’s interested in all aspects of
+            design, from sketching, to wireframing and final look and feel.
+            Music and photography are his other passions. Check out the resume
+            below.
+          </p>
+        </div>
+        <div class="p-16 text-xl">
+          <p>
+            Lucas Czuchraj is a designer from Toronto. He has 15+ years of
+            experience in the field, graduated from the York Sheridan Bachelor
+            of Design (Honors) program. He’s interested in all aspects of
+            design, from sketching, to wireframing and final look and feel.
+            Music and photography are his other passions. Check out the resume
+            below.
+          </p>
+        </div>
       </div>
     </div>
 
-    <div class="grid grid-cols-3 gap-1 p-16 mx-auto">
+    <div class="grid grid-cols-5 gap-0 mx-auto">
       <div>
         <img
-          class="overflow-hidden bg-white shadow-2xl"
+          class="object-cover w-full overflow-hidden transition duration-300 ease-out transform shadow-2xl h-72 hover:-translate-y-1 hover:scale-95"
           src="https://images.unsplash.com/photo-1496016943515-7d33598c11e6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80"
         />
       </div>
       <div>
         <img
-          class="overflow-hidden bg-white shadow-2xl"
+          class="object-cover w-full overflow-hidden transition duration-300 ease-out transform shadow-2xl h-72 hover:-translate-y-1 hover:scale-95"
           src="https://images.unsplash.com/photo-1615836364937-786654db8232?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
         />
       </div>
       <div>
         <img
-          class="overflow-hidden bg-white shadow-2xl"
-          src="https://images.unsplash.com/photo-1496016943515-7d33598c11e6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80"
+          class="object-cover w-full overflow-hidden transition duration-300 ease-out transform shadow-2xl h-72 hover:-translate-y-1 hover:scale-95"
+          src="https://images.unsplash.com/photo-1615886490002-ae8172eea2de?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=660&q=80"
         />
       </div>
       <div>
         <img
-          class="overflow-hidden bg-white shadow-2xl"
-          src="https://images.unsplash.com/photo-1496016943515-7d33598c11e6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80"
+          class="object-cover w-full overflow-hidden transition duration-300 ease-out transform shadow-2xl h-72 hover:-translate-y-1 hover:scale-95"
+          src="https://images.unsplash.com/photo-1615884363252-983eed162938?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
         />
       </div>
       <div>
         <img
-          class="overflow-hidden bg-white shadow-2xl"
-          src="https://images.unsplash.com/photo-1496016943515-7d33598c11e6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80"
+          class="object-cover w-full overflow-hidden transition duration-300 ease-out transform shadow-2xl h-72 hover:-translate-y-1 hover:scale-95"
+          src="https://images.unsplash.com/photo-1615829913763-bc72584f166f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80"
         />
       </div>
       <div>
         <img
-          class="overflow-hidden bg-white shadow-2xl"
-          src="https://images.unsplash.com/photo-1496016943515-7d33598c11e6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80"
+          class="object-cover w-full overflow-hidden transition duration-300 ease-out transform shadow-2xl h-72 hover:-translate-y-1 hover:scale-95"
+          src="https://images.unsplash.com/photo-1615847697785-effb9d96a274?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1339&q=80"
         />
       </div>
       <div>
         <img
-          class="overflow-hidden bg-white shadow-2xl"
-          src="https://images.unsplash.com/photo-1496016943515-7d33598c11e6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80"
+          class="object-cover w-full overflow-hidden transition duration-300 ease-out transform shadow-2xl h-72 hover:-translate-y-1 hover:scale-95"
+          src="https://images.unsplash.com/photo-1611095789391-947c8f94179c?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80"
         />
       </div>
       <div>
         <img
-          class="overflow-hidden bg-white shadow-2xl"
-          src="https://images.unsplash.com/photo-1496016943515-7d33598c11e6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80"
+          class="object-cover w-full overflow-hidden transition duration-300 ease-out transform shadow-2xl h-72 hover:-translate-y-1 hover:scale-95"
+          src="https://images.unsplash.com/photo-1615872862502-f1b3aede3a73?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80"
         />
       </div>
       <div>
         <img
-          class="overflow-hidden bg-white shadow-2xl"
-          src="https://images.unsplash.com/photo-1496016943515-7d33598c11e6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80"
+          class="object-cover w-full overflow-hidden transition duration-300 ease-out transform shadow-2xl h-72 hover:-translate-y-1 hover:scale-95"
+          src="https://images.unsplash.com/photo-1615827806874-212122b513f4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
         />
+      </div>
+    </div>
+    <div class="h-full p-16 bg-gray-400">
+      <div class="flex">
+        <div class="p-16 text-4xl leading-relaxed">
+          <p>
+            Lucas Czuchraj is a designer from Toronto. He has 15+ years of
+            experience in the field, graduated from the York Sheridan Bachelor
+            of Design (Honors) program. He’s interested in all aspects of
+            design, from sketching, to wireframing and final look and feel.
+            Music and photography are his other passions. Check out the resume
+            below.
+          </p>
+        </div>
+        <div class="p-16 text-xl">
+          <p>
+            Lucas Czuchraj is a designer from Toronto. He has 15+ years of
+            experience in the field, graduated from the York Sheridan Bachelor
+            of Design (Honors) program. He’s interested in all aspects of
+            design, from sketching, to wireframing and final look and feel.
+            Music and photography are his other passions. Check out the resume
+            below.
+          </p>
+        </div>
+        <div class="p-16 text-xl">
+          <p>
+            Lucas Czuchraj is a designer from Toronto. He has 15+ years of
+            experience in the field, graduated from the York Sheridan Bachelor
+            of Design (Honors) program. He’s interested in all aspects of
+            design, from sketching, to wireframing and final look and feel.
+            Music and photography are his other passions. Check out the resume
+            below.
+          </p>
+          <button
+            type="button"
+            class="inline-flex items-center px-6 py-3 mt-6 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            Say hello!
+          </button>
+        </div>
       </div>
     </div>
   </div>
