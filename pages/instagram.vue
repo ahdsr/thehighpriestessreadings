a1a687fbcf8c284b8984cabc2376f4170169a15d
ahdsr <lucas.czuchraj@gmail.com>
2021-03-16 16:30:17 -0400

    Instagram

---
+++ pages/instagram.vue
@@ -0,0 +1,233 @@
+<template>
+  <div class="bg-black">
+    <div class="p-4 mx-auto text-white">
+      <img
+        class="w-24 text-white fill-current"
+        viewBox="0 0 20 20"
+        src="~/assets/images/Instagram_logo.svg"
+      />
+    </div>
+    <!-- Users -->
+    <div class="flex p-4 mx-auto -mt-3">
+      <div>
+        <span class="relative inline-block pr-5 text-xs text-white">
+          <img
+            class="rounded-full h-14 w-14"
+            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixqx=MnLvkwmKZ1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+            alt=""
+          />
+          <span>Your Story</span>
+          <span
+            class="absolute bottom-4 left-11 block h-3.5 w-3.5 rounded-full ring-2 ring-white bg-red-400"
+          ></span>
+        </span>
+      </div>
+      <div>
+        <span class="relative inline-block pr-5 text-xs text-white">
+          <img
+            class="rounded-full h-14 w-14"
+            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixqx=MnLvkwmKZ1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+            alt=""
+          />
+          <span>Your Story</span>
+          <span
+            class="absolute bottom-4 left-11 block h-3.5 w-3.5 rounded-full ring-2 ring-white bg-red-400"
+          ></span>
+        </span>
+      </div>
+      <div>
+        <span class="relative inline-block pr-5 text-xs text-white">
+          <img
+            class="rounded-full h-14 w-14"
+            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixqx=MnLvkwmKZ1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+            alt=""
+          />
+          <span>Your Story</span>
+          <span
+            class="absolute bottom-4 left-11 block h-3.5 w-3.5 rounded-full ring-2 ring-white bg-red-400"
+          ></span>
+        </span>
+      </div>
+    </div>
+    <!-- Post 1 -->
+    <div>
+      <div class="text-white">
+        <div class="p-2 mx-auto">
+          <span
+            class="relative inline-block text-xs text-white align-middle text-bold"
+          >
+            <img
+              class="w-8 h-8 rounded-full"
+              src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixqx=MnLvkwmKZ1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+              alt=""
+            />
+            <span
+              class="absolute bottom-0 right-0 block w-2 h-2 bg-red-400 rounded-full ring-2 ring-white"
+            ></span> </span
+          ><span class="ml-2 text-xs text-white text-bold"
+            >life.with.my.three</span
+          >
+        </div>
+        <img
+          class="object-cover w-full mt-2 h-60"
+          src="https://images.unsplash.com/photo-1615836364937-786654db8232?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
+          alt=""
+        />
+        <div class="flex p-2 mx-auto">
+          <a href="#"
+            ><svg
+              class="w-6 h-6 fill-current"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+              /></svg
+          ></a>
+          <a href="#"
+            ><svg
+              class="w-6 h-6 fill-current"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+              /></svg
+          ></a>
+          <a href="#"
+            ><svg
+              class="w-6 h-6 fill-current"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+              /></svg
+          ></a>
+        </div>
+      </div>
+    </div>
+    <!-- Post 2 -->
+    <div>
+      <div class="text-white">
+        <div class="p-2 mx-auto">
+          <span
+            class="relative inline-block text-xs text-white align-middle text-bold"
+          >
+            <img
+              class="w-8 h-8 rounded-full"
+              src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixqx=MnLvkwmKZ1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+              alt=""
+            />
+            <span
+              class="absolute bottom-0 right-0 block w-2 h-2 bg-red-400 rounded-full ring-2 ring-white"
+            ></span> </span
+          ><span class="ml-2 text-xs text-white text-bold"
+            >life.with.my.three</span
+          >
+        </div>
+        <img
+          class="object-cover w-full mt-2 h-60"
+          src="https://images.unsplash.com/photo-1615836364937-786654db8232?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
+          alt=""
+        />
+        <div class="flex p-2 mx-auto">
+          <a href="#"
+            ><svg
+              class="w-6 h-6 fill-current"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+              /></svg
+          ></a>
+          <a href="#"
+            ><svg
+              class="w-6 h-6 fill-current"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+              /></svg
+          ></a>
+          <a href="#"
+            ><svg
+              class="w-6 h-6 fill-current"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+              /></svg
+          ></a>
+        </div>
+      </div>
+    </div>
+    <!-- Post 3 -->
+    <div>
+      <div class="text-white">
+        <div class="p-2 mx-auto">
+          <span
+            class="relative inline-block text-xs text-white align-middle text-bold"
+          >
+            <img
+              class="w-8 h-8 rounded-full"
+              src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixqx=MnLvkwmKZ1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+              alt=""
+            />
+            <span
+              class="absolute bottom-0 right-0 block w-2 h-2 bg-red-400 rounded-full ring-2 ring-white"
+            ></span> </span
+          ><span class="ml-2 text-xs text-white text-bold"
+            >life.with.my.three</span
+          >
+        </div>
+        <img
+          class="object-cover w-full mt-2 h-60"
+          src="https://images.unsplash.com/photo-1615836364937-786654db8232?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
+          alt=""
+        />
+        <div class="flex p-2 mx-auto">
+          <a href="#"
+            ><svg
+              class="w-6 h-6 fill-current"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+              /></svg
+          ></a>
+          <a href="#"
+            ><svg
+              class="w-6 h-6 fill-current"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+              /></svg
+          ></a>
+          <a href="#"
+            ><svg
+              class="w-6 h-6 fill-current"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+              /></svg
+          ></a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
