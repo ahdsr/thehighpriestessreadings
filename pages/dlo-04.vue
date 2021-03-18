c4dec9deb2be2046bfd795d67ebef184c27bc06a
ahdsr <lucas.czuchraj@gmail.com>
2021-03-18 13:33:15 -0400

    Homepage

--- pages/dlo-04.vue
+++ pages/dlo-04.vue
@@ -71,27 +71,29 @@
             <div class="absolute inset-0 flex items-center" aria-hidden="true">
               <div class="h-0.5 w-full bg-element-bright-blue"></div>
             </div>
-            <a
-              href="#"
-              class="relative flex items-center justify-center w-8 h-8 rounded-full bg-element-green hover:bg-indigo-900"
-            >
-              <!-- Heroicon name: solid/check -->
-              <svg
-                class="w-5 h-5 text-white"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                aria-hidden="true"
+            <nuxt-link to="/dlo-01"
+              ><a
+                href="#"
+                class="relative flex items-center justify-center w-8 h-8 rounded-full bg-element-green hover:bg-indigo-900"
               >
-                <path
-                  fill-rule="evenodd"
-                  d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                  clip-rule="evenodd"
-                />
-              </svg>
+                <!-- Heroicon name: solid/check -->
+                <svg
+                  class="w-5 h-5 text-white"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
 
-              <span class="sr-only">Step 1</span>
-            </a>
+                <span class="sr-only">Step 1</span>
+              </a></nuxt-link
+            >
           </li>
 
           <li class="relative pr-8 sm:pr-20">
@@ -99,27 +101,29 @@
             <div class="absolute inset-0 flex items-center" aria-hidden="true">
               <div class="h-0.5 w-full bg-element-bright-blue"></div>
             </div>
-            <a
-              href="#"
-              class="relative flex items-center justify-center w-8 h-8 rounded-full bg-element-green hover:bg-indigo-900"
-            >
-              <!-- Heroicon name: solid/check -->
-              <svg
-                class="w-5 h-5 text-white"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                aria-hidden="true"
+            <nuxt-link to="/dlo-02"
+              ><a
+                href="#"
+                class="relative flex items-center justify-center w-8 h-8 rounded-full bg-element-green hover:bg-indigo-900"
               >
-                <path
-                  fill-rule="evenodd"
-                  d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                  clip-rule="evenodd"
-                />
-              </svg>
+                <!-- Heroicon name: solid/check -->
+                <svg
+                  class="w-5 h-5 text-white"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
 
-              <span class="sr-only">Step 1</span>
-            </a>
+                <span class="sr-only">Step 1</span>
+              </a></nuxt-link
+            >
           </li>
 
           <li class="relative pr-8 sm:pr-20">
@@ -127,27 +131,29 @@
             <div class="absolute inset-0 flex items-center" aria-hidden="true">
               <div class="h-0.5 w-full bg-element-bright-blue"></div>
             </div>
-            <a
-              href="#"
-              class="relative flex items-center justify-center w-8 h-8 rounded-full bg-element-green hover:bg-indigo-900"
-            >
-              <!-- Heroicon name: solid/check -->
-              <svg
-                class="w-5 h-5 text-white"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                aria-hidden="true"
+            <nuxt-link to="/dlo-03"
+              ><a
+                href="#"
+                class="relative flex items-center justify-center w-8 h-8 rounded-full bg-element-green hover:bg-indigo-900"
               >
-                <path
-                  fill-rule="evenodd"
-                  d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                  clip-rule="evenodd"
-                />
-              </svg>
+                <!-- Heroicon name: solid/check -->
+                <svg
+                  class="w-5 h-5 text-white"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
 
-              <span class="sr-only">Step 1</span>
-            </a>
+                <span class="sr-only">Step 1</span>
+              </a></nuxt-link
+            >
           </li>
 
           <li class="relative pr-8 sm:pr-20">
@@ -155,17 +161,19 @@
             <div class="absolute inset-0 flex items-center" aria-hidden="true">
               <div class="h-0.5 w-full bg-gray-200"></div>
             </div>
-            <a
-              href="#"
-              class="relative flex items-center justify-center w-8 h-8 bg-white border-2 rounded-full border-element-green"
-              aria-current="step"
+            <nuxt-link to="/dlo-04"
+              ><a
+                href="#"
+                class="relative flex items-center justify-center w-8 h-8 bg-white border-2 rounded-full border-element-green"
+                aria-current="step"
+              >
+                <span
+                  class="h-2.5 w-2.5 bg-indigo-600 rounded-full"
+                  aria-hidden="true"
+                ></span>
+                <span class="sr-only">Step 2</span>
+              </a></nuxt-link
             >
-              <span
-                class="h-2.5 w-2.5 bg-indigo-600 rounded-full"
-                aria-hidden="true"
-              ></span>
-              <span class="sr-only">Step 2</span>
-            </a>
           </li>
 
           <li class="relative pr-8 sm:pr-20">
@@ -173,32 +181,36 @@
             <div class="absolute inset-0 flex items-center" aria-hidden="true">
               <div class="h-0.5 w-full bg-gray-200"></div>
             </div>
-            <a
-              href="#"
-              class="relative flex items-center justify-center w-8 h-8 bg-white border-2 border-gray-300 rounded-full group hover:border-gray-400"
+            <nuxt-link to="/dlo-05"
+              ><a
+                href="#"
+                class="relative flex items-center justify-center w-8 h-8 bg-white border-2 border-gray-300 rounded-full group hover:border-gray-400"
+              >
+                <span
+                  class="h-2.5 w-2.5 bg-transparent rounded-full group-hover:bg-gray-300"
+                  aria-hidden="true"
+                ></span>
+                <span class="sr-only">Step 5</span>
+              </a></nuxt-link
             >
-              <span
-                class="h-2.5 w-2.5 bg-transparent rounded-full group-hover:bg-gray-300"
-                aria-hidden="true"
-              ></span>
-              <span class="sr-only">Step 5</span>
-            </a>
           </li>
           <li class="relative">
             <!-- Upcoming Step -->
             <div class="absolute inset-0 flex items-center" aria-hidden="true">
               <div class="h-0.5 w-full bg-gray-200"></div>
             </div>
-            <a
-              href="#"
-              class="relative flex items-center justify-center w-8 h-8 bg-white border-2 border-gray-300 rounded-full group hover:border-gray-400"
+            <nuxt-link to="/dlo-06"
+              ><a
+                href="#"
+                class="relative flex items-center justify-center w-8 h-8 bg-white border-2 border-gray-300 rounded-full group hover:border-gray-400"
+              >
+                <span
+                  class="h-2.5 w-2.5 bg-transparent rounded-full group-hover:bg-gray-300"
+                  aria-hidden="true"
+                ></span>
+                <span class="sr-only">Step 5</span>
+              </a></nuxt-link
             >
-              <span
-                class="h-2.5 w-2.5 bg-transparent rounded-full group-hover:bg-gray-300"
-                aria-hidden="true"
-              ></span>
-              <span class="sr-only">Step 5</span>
-            </a>
           </li>
         </ol>
       </nav>
