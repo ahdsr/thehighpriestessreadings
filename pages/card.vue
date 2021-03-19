61481a4d2be03b33cccaf6894ce10ec55242dc15
ahdsr <lucas.czuchraj@gmail.com>
2021-03-19 15:47:48 -0400

    Firebase

--- pages/card.vue
+++ pages/card.vue
@@ -1,64 +1,13 @@
 <template>
-  <div id="app" class="font-mono antialiased text-gray-900">
-    <h1 class="font-serif text-4xl">This is responsive!</h1>
+  <div id="app" class="antialiased text-gray-900">
+    <h1 class="text-xl">Choose your recipe</h1>
 
-    <div class="items-center min-h-screen p-12">
+    <div class="items-center min-h-screen p-4">
       <propertyCard :property="property" />
       <propertyCard :property="property2" />
       <propertyCard :property="property3" />
       <propertyCard :property="property4" />
     </div>
-
-
-<div id="accordion">
-  <h3>Section 1</h3>
-  <div>
-    <p>
-    Mauris mauris ante, blandit et, ultrices a, suscipit eget, quam. Integer
-    ut neque. Vivamus nisi metus, molestie vel, gravida in, condimentum sit
-    amet, nunc. Nam a nibh. Donec suscipit eros. Nam mi. Proin viverra leo ut
-    odio. Curabitur malesuada. Vestibulum a velit eu ante scelerisque vulputate.
-    </p>
-  </div>
-  <h3>Section 2</h3>
-  <div>
-    <p>
-    Sed non urna. Donec et ante. Phasellus eu ligula. Vestibulum sit amet
-    purus. Vivamus hendrerit, dolor at aliquet laoreet, mauris turpis porttitor
-    velit, faucibus interdum tellus libero ac justo. Vivamus non quam. In
-    suscipit faucibus urna.
-    </p>
-  </div>
-  <h3>Section 3</h3>
-  <div>
-    <p>
-    Nam enim risus, molestie et, porta ac, aliquam ac, risus. Quisque lobortis.
-    Phasellus pellentesque purus in massa. Aenean in pede. Phasellus ac libero
-    ac tellus pellentesque semper. Sed ac felis. Sed commodo, magna quis
-    lacinia ornare, quam ante aliquam nisi, eu iaculis leo purus venenatis dui.
-    </p>
-    <ul>
-      <li>List item one</li>
-      <li>List item two</li>
-      <li>List item three</li>
-    </ul>
-  </div>
-  <h3>Section 4</h3>
-  <div>
-    <p>
-    Cras dictum. Pellentesque habitant morbi tristique senectus et netus
-    et malesuada fames ac turpis egestas. Vestibulum ante ipsum primis in
-    faucibus orci luctus et ultrices posuere cubilia Curae; Aenean lacinia
-    mauris vel est.
-    </p>
-    <p>
-    Suspendisse eu nisl. Nullam ut libero. Integer dignissim consequat lectus.
-    Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
-    inceptos himenaeos.
-    </p>
-  </div>
-</div>
-
   </div>
 </template>
 
@@ -76,10 +25,10 @@ export default {
   },
   head() {
     return {
-      script: [{ src: 'https://code.jquery.com/jquery-1.12.4.js'}],
-      script: [{ src: 'https://code.jquery.com/ui/1.12.1/jquery-ui.js'}],
-
-      link: [{ rel: 'stylesheet', href: 'path/to/your/css/file.css' }],
+ //     script: [{ src: 'https://code.jquery.com/jquery-1.12.4.js'}],
+ //     script: [{ src: 'https://code.jquery.com/ui/1.12.1/jquery-ui.js'}],
+//
+      link: [{ rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Tangerine">' }],
     }
   },
   data() {
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,13 +10,13 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      display: ['Literata'],
+    },
     extend: {
-      fontFamily: {
-        sans: ['Inter var', defaultTheme.fontFamily.sans],
-      },
-      backgroundImage: (theme) => ({
+       backgroundImage: (theme) => ({
         'hero-pattern':
-          "url('https://images.unsplash.com/photo-1496016943515-7d33598c11e6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80')",
+          "url('https://images.unsplash.com/photo-1607893240314-91143f82a0ce?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80')",
 
         'footer-texture':
           "url('https://images.unsplash.com/photo-1496016943515-7d33598c11e6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80')",
