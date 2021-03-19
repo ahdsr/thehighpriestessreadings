d726485b22c2d9ceb069636c20f8be736adaaa98
ahdsr <lucas.czuchraj@gmail.com>
2021-03-19 11:53:23 -0400

    asdsad

--- pages/card.vue
+++ pages/card.vue
@@ -8,11 +8,66 @@
       <propertyCard :property="property3" />
       <propertyCard :property="property4" />
     </div>
+
+
+<div id="accordion">
+  <h3>Section 1</h3>
+  <div>
+    <p>
+    Mauris mauris ante, blandit et, ultrices a, suscipit eget, quam. Integer
+    ut neque. Vivamus nisi metus, molestie vel, gravida in, condimentum sit
+    amet, nunc. Nam a nibh. Donec suscipit eros. Nam mi. Proin viverra leo ut
+    odio. Curabitur malesuada. Vestibulum a velit eu ante scelerisque vulputate.
+    </p>
+  </div>
+  <h3>Section 2</h3>
+  <div>
+    <p>
+    Sed non urna. Donec et ante. Phasellus eu ligula. Vestibulum sit amet
+    purus. Vivamus hendrerit, dolor at aliquet laoreet, mauris turpis porttitor
+    velit, faucibus interdum tellus libero ac justo. Vivamus non quam. In
+    suscipit faucibus urna.
+    </p>
+  </div>
+  <h3>Section 3</h3>
+  <div>
+    <p>
+    Nam enim risus, molestie et, porta ac, aliquam ac, risus. Quisque lobortis.
+    Phasellus pellentesque purus in massa. Aenean in pede. Phasellus ac libero
+    ac tellus pellentesque semper. Sed ac felis. Sed commodo, magna quis
+    lacinia ornare, quam ante aliquam nisi, eu iaculis leo purus venenatis dui.
+    </p>
+    <ul>
+      <li>List item one</li>
+      <li>List item two</li>
+      <li>List item three</li>
+    </ul>
+  </div>
+  <h3>Section 4</h3>
+  <div>
+    <p>
+    Cras dictum. Pellentesque habitant morbi tristique senectus et netus
+    et malesuada fames ac turpis egestas. Vestibulum ante ipsum primis in
+    faucibus orci luctus et ultrices posuere cubilia Curae; Aenean lacinia
+    mauris vel est.
+    </p>
+    <p>
+    Suspendisse eu nisl. Nullam ut libero. Integer dignissim consequat lectus.
+    Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
+    inceptos himenaeos.
+    </p>
+  </div>
+</div>
+
   </div>
 </template>
 
 <script>
 import propertyCard from '~/components/propertyCard'
+
+//$( ".accordion" ).accordion();
+
+
 
 export default {
   name: 'app',
@@ -21,7 +76,9 @@ export default {
   },
   head() {
     return {
-      script: [{ src: 'path/to/your/js/file.js' }],
+      script: [{ src: 'https://code.jquery.com/jquery-1.12.4.js'}],
+      script: [{ src: 'https://code.jquery.com/ui/1.12.1/jquery-ui.js'}],
+
       link: [{ rel: 'stylesheet', href: 'path/to/your/css/file.css' }],
     }
   },
@@ -69,7 +126,11 @@ export default {
       },
     }
   },
+  mounted(){
+
+  }
+
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style></style>
