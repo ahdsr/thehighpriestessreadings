04806a5e5860e6ff846909b72366ead1a6189c68
ahdsr <lucas.czuchraj@gmail.com>
2021-03-27 00:20:18 -0400

    Fonts

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,6 @@ export default {
         rel: 'stylesheet',
         href:
           'https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.css',
-          'https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap',
       },
     ],
   },
