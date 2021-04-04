401e86fc464de190499149557dc9664ff35f30c5
ahdsr <lucas.czuchraj@gmail.com>
2021-04-03 21:34:32 -0400

    All good

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,15 @@
 <template>
   <div>
+    <canvas id="canvas" class="bg-center bg-hero-monica-09">
+      <div class="h-screen">asasdas</div>
+    </canvas>
     <div
       id="navbar"
       class="fixed top-0 z-50 w-screen border-b border-blue-100 bg-monica-mauvelous"
     >
       <topNavigation />
     </div>
-
+    <!-- Hero -->
     <div
       id="box"
       class="h-screen text-center bg-center rounded-4xl bg-hero-monica-08 sm:bg-cover lg:bg-center xl:bg-hero-monica-01 xl:rounded-4xl bgeffect"
@@ -46,22 +49,22 @@
         </div>
       </nuxt-link>
     </div>
-
-    <div id="box2" class="font-serif">
+    <!-- About Readings -->
+    <div id="readings" class="font-serif">
       <div class="max-w-4xl pt-20 mx-auto"></div>
       <sectionReadings />
     </div>
-
-    <div id="box2" class="font-serif">
+    <!-- Pricing -->
+    <div id="pricing" class="font-serif">
       <div class="max-w-4xl pt-20 mx-auto"></div>
       <sectionPricing />
     </div>
-
-    <div id="box3" class="font-serif">
+    <!-- Testimonials -->
+    <div id="tesimonials" class="font-serif">
       <sectionTestimonial />
     </div>
-
-    <div id="box4" class="font-serif">
+    <!-- About Monica -->
+    <div id="aboutmonica" class="font-serif">
       <sectionAbout />
     </div>
   </div>
@@ -70,12 +73,118 @@
 <script>
 export default {
   mounted() {
-    this.navigationIn()
-    this.heroline1()
-    this.heroline2()
+    var Canvas = document.getElementById('canvas')
+    var ctx = Canvas.getContext('2d')
 
-    this.animateOnScroll()
-    // this.navbar()
+    var resize = function () {
+      Canvas.width = Canvas.clientWidth
+      Canvas.height = Canvas.clientHeight
+    }
+    window.addEventListener('resize', resize)
+    resize()
+
+    var elements = []
+    var presets = {}
+
+    presets.o = function (x, y, s, dx, dy) {
+      return {
+        x: x,
+        y: y,
+        r: 12 * s,
+        w: 5 * s,
+        dx: dx,
+        dy: dy,
+        draw: function (ctx, t) {
+          this.x += this.dx
+          this.y += this.dy
+
+          ctx.beginPath()
+          ctx.arc(
+            this.x + +Math.sin((50 + x + t / 10) / 100) * 3,
+            this.y + +Math.sin((45 + x + t / 10) / 100) * 4,
+            this.r,
+            0,
+            2 * Math.PI,
+            false
+          )
+          ctx.lineWidth = this.w
+          ctx.strokeStyle = '#fff'
+          ctx.stroke()
+        },
+      }
+    }
+
+    presets.x = function (x, y, s, dx, dy, dr, r) {
+      r = r || 0
+      return {
+        x: x,
+        y: y,
+        s: 20 * s,
+        w: 5 * s,
+        r: r,
+        dx: dx,
+        dy: dy,
+        dr: dr,
+        draw: function (ctx, t) {
+          this.x += this.dx
+          this.y += this.dy
+          this.r += this.dr
+
+          var _this = this
+          var line = function (x, y, tx, ty, c, o) {
+            o = o || 0
+            ctx.beginPath()
+            ctx.moveTo(-o + (_this.s / 2) * x, o + (_this.s / 2) * y)
+            ctx.lineTo(-o + (_this.s / 2) * tx, o + (_this.s / 2) * ty)
+            ctx.lineWidth = _this.w
+            ctx.strokeStyle = c
+            ctx.stroke()
+          }
+
+          ctx.save()
+
+          ctx.translate(
+            this.x + Math.sin((x + t / 10) / 100) * 5,
+            this.y + Math.sin((10 + x + t / 10) / 100) * 2
+          )
+          ctx.rotate((this.r * Math.PI) / 180)
+
+          line(-1, -1, 1, 1, '#fff')
+          line(1, -1, -1, 1, '#fff')
+
+          ctx.restore()
+        },
+      }
+    }
+
+    for (var x = 0; x < Canvas.width; x++) {
+      for (var y = 0; y < Canvas.height; y++) {
+        if (Math.round(Math.random() * 8000) == 1) {
+          var s = (Math.random() * 5 + 1) / 10
+          if (Math.round(Math.random()) == 1)
+            elements.push(presets.o(x, y, s, 0, 0))
+          else
+            elements.push(
+              presets.x(
+                x,
+                y,
+                s,
+                0,
+                0,
+                (Math.random() * 3 - 1) / 10,
+                Math.random() * 360
+              )
+            )
+        }
+      }
+    }
+
+    setInterval(function () {
+      ctx.clearRect(0, 0, Canvas.width, Canvas.height)
+
+      var time = new Date().getTime()
+      for (var e in elements) elements[e].draw(ctx, time)
+    }, 10)
   },
 
   methods: {
@@ -129,7 +238,7 @@ export default {
         scrollTrigger: {
           trigger: '#box',
           pin: false,
-          start: '+=50px',
+          start: '+=100px',
           end: 'bottom',
           scrub: 0.4,
           invalidateOnRefresh: true,
@@ -138,7 +247,7 @@ export default {
         },
       })
 
-      this.$gsap.from('#box2', {
+      this.$gsap.from('#readings', {
         scaleX: 1.2,
         scaleY: 1.2,
         y: 200,
@@ -154,7 +263,7 @@ export default {
         },
       })
 
-      this.$gsap.from('#box3', {
+      this.$gsap.from('#pricing', {
         scaleX: 1.2,
         scaleY: 1.2,
         y: 200,
